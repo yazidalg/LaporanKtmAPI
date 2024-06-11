af33bd7774e1cb23829407569a2dd2621ba72429
yazidalg <yazid.al2418@gmail.com>
2024-06-12 02:24:03 +0700

    first blood

---
+++ LaporanKtmAPI-v1.0.0/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿namespace LaporanKtmAPI_v1._0._0;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
+
